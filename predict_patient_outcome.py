a4a7105a0057c1cdd9fc4685800f5aa070654ecc
AssafR <assaf@razon-family.com>
2018-12-22 21:36:35 +0200

    Added Jupyter notebook file

--- predict_patient_outcome.py
+++ predict_patient_outcome.py
@@ -1,1 +1,13 @@
-#
+import pandas as pd
+
+def read_from_csv(csvfile):
+    """ Read from csv file and convert all values in scientific format to integer"""
+    df = pd.read_csv(csvfile,header=None,names=['feature1','feature2','feature3','feature4','feature5','feature6','outcome'])
+    return df
+pd.set_option('display.expand_frame_repr', False)
+
+
+csvFile = 'ES_DS_test_section1.csv'
+df = read_from_csv(csvFile)
+df = df.astype({'outcome':int})
+print(df.describe())
